515750bd2e83f661a0150ffb004b90a2ac29acf1
Anastasia Tsypkaykina <tsypkaykina.anastasia@gmail.com>
2025-08-22 16:10:09 +0200

    changes in menu

---
+++ menu.js
@@ -0,0 +1,71 @@
+let dishes = [
+        {
+            "Pizza": [
+                {
+                    "Name": "Pizza Prosciutto",
+                    "Ingredients": "Herzhafter, zarter Schinken trifft auf cremigen Mozzarella und eine aromatische Tomatensauce - ein klassischer Genuss für alle, die den Geschmack der Tradition lieben.",
+                    "Price": 12.00
+                },
+                {
+                    "Name": "Pizza Salami",
+                    "Ingredients": "Würzige Salami trifft auf schmelzenden Mozzarella und aromatische Tomatensauce - ein wahrer Genuss für alle, die es herzhaft mögen.",
+                    "Price": 13.90
+                },
+                {
+                    "Name": "Pizza Tonno",
+                    "Ingredients": "Thunfisch, Zwiebel und Kapern",
+                    "Price": 12.50
+                },
+                {
+                    "Name": "Pizza Margherita",
+                    "Ingredients": "Eine klassische, italienische Pizza, die mit aromatischer Tomatensauce, zart schmelzendem Mozzarella und frischem Basilikum belegt ist. Einfach, aber ein wahres Meisterwerk der italienischen Küche.",
+                    "Price": 11.00
+                },
+                {
+                    "Name": "Pizza Provinciale",
+                    "Ingredients": "Eine leckere Kombination aus würziger Salami und zartem Schinken, die zusammen mit Mozzarella und Tomatensauce zu einer Geschmacksexplosion führt.",
+                    "Price": 13.30
+                },
+                {
+                    "Name": "Pizza Quattro Formaggi",
+                    "Ingredients": "Eine verführerische Mischung aus vier verschiedenen Käsesorten, die zusammen zu einem reichen, cremigen und vollmundigen Geschmack verschmelzen - der wahre Traum für Käseliebhaber.",
+                    "Price": 12.00
+                },
+                {
+                    "Name": "Pizza mit Meeresfrüchten",
+                    "Ingredients": "Mit einer großzügigen Auswahl an frischen Meeresfrüchten wie Garnelen und Muscheln, perfekt in Kombination mit einer Tomatenbasis und schmelzendem Mozzarella - ein wahres Fest für Liebhaber des Ozeans.",
+                    "Price": 13.00
+                },
+                { 
+                    "Name": "Pizza Calzone",
+                    "Ingredients": "Zarter Schinken und frische Pilze kommen auf dieser Pizza zusammen, um in perfektem Einklang mit der Tomatensauce und Mozzarella zu schmecken.",
+                    "Price": 12.50
+                },                 
+            ]
+        },    
+        {
+            "Pasta": [
+                {
+                    "Name": "Pasta Bolognese",
+                    "Ingredients": "mit Fleischsauce",
+                    "Price": 11.40
+                },
+                {
+                    "Name": "Pasta mit Pesto",
+                    "Ingredients": "mit Rucola, Basilikum und Käse",
+                    "Price": 10.20
+                },
+                {
+                    "Name": "Pasta Carbonara",
+                    "Ingredients": "mit Ei, Speck und Parmesan",
+                    "Price": 11.50
+                },                
+            ]
+        },    
+            
+];
+
+
+
+
+
